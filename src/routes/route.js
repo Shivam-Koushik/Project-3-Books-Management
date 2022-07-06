e6cf9740268cfa9acd3ccee4e7c6466f5bdd99b4
shivani-0515 <shivanichaudhary05072001@gmail.com>
2022-07-06 20:07:48 +0530

    modified

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,7 @@ const bookController = require('../controllers/bookController')
 router.post('/register',userController.register)
 router.post('/login',userController.login)
 router.post('/books',bookController.postBooks)
-
+router.get('/books',bookController.getBooks)
 
 
 module.exports = router;
